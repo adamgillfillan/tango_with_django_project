28073ddb6464681c22664148cf1f3cebffcd9879
Adam Gillfillan <agillfi@ncsu.edu>
2014-02-26 00:20:16 -0500

    added hello world

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^rango/', include('rango.urls')),  # ADD THIS NEW TUPLE!
+                       )
