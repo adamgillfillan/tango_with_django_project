bd25618b32da753e4510b98eca7cfacfa3037d27
Adam Gillfillan <agillfi@ncsu.edu>
2014-02-27 16:28:52 -0500

    updated models and populate script

--- populate_rango.py
+++ populate_rango.py
@@ -2,8 +2,8 @@ __author__ = 'Adam'
 import os
 
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 
@@ -13,7 +13,7 @@ def add_page(cat, title, url, views=0):
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', 128, 64)
 
     add_page(cat=python_cat,
              title="Official Python Tutorial",
@@ -27,7 +27,7 @@ def populate():
              title="Learn Python in 10 Minutes",
              url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
              title="Official Django Tutorial",
@@ -41,7 +41,7 @@ def populate():
              title="How to Tango with Django",
              url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", 32, 16)
 
     add_page(cat=frame_cat,
              title="Bottle",
