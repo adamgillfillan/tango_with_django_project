3420438201ada1576f411599d89e62a6662ed362
Adam Gillfillan <agillfi@ncsu.edu>
2014-02-28 01:55:58 -0500

    added category view

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -28,3 +28,38 @@ def about(request):
     context_dict = {'sillymessage': "Hello lol."}
 
     return render_to_response('rango/about.html', context_dict, context)
+
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    # Change underscores in the category name to spaces.
+    # URLs don't handle spaces well, so we encode them as underscores.
+    # We can then simply replace the underscores with spaces again to get the name.
+    category_name = category_name_url.replace('_', ' ')
+
+    context_dict = {'category_name': category_name}
+
+    try:
+        # Can we find a category with the given name?
+        # If we can't, the .get() method raises a DoesNotExist exception.
+        # So the .get() method returns one model instance or raises an exception.
+        my_category = Category.objects.get(name=category_name)
+
+        # Retrieve all of the associated pages.
+        # Note that filter returns >= 1 model instance.
+        pages = Page.objects.filter(category=my_category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = my_category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category.
+        # Don't do anything - the template displays the "no category" message for us.
+        pass
+
+    # Go render the response and return it to the client.
+    return render_to_response('rango/category.html', context_dict, context)
